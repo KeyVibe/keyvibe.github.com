2055298884e5c192189400ff29094837c0f1e616
Betsy Pena <blpg@hotmail.com>
2011-11-16 15:56:21 -0500

    script.js

---
+++ miScript.js
@@ -0,0 +1,98 @@
+$(document).ready(function(){
+	$('#carrito').hide();
+	
+	$('#comprar').click(function(){
+		
+		$('#a').hover(
+		function(){ $(this).addClass('hover')},
+		function(){ $(this).removeClass('hover');}
+		);
+		
+		
+		var x=$('#cant1').val();
+		var y=$('#cant2').val();
+		var z=$('#cant3').val();
+		var e=$('#cant4').val();
+		var r=$('#cant5').val();
+		console.log(x);
+		
+		$('#containerCap').hide();
+		$('#carrito').show();
+		
+		 if( x == 0){
+			x=0; 
+		}
+		if( y == 0){
+			y=0; 
+		}
+		if( z == 0){
+			z=0; 
+		}
+		if( e == 0){
+			e=0; 
+		}
+		if( r == 0){
+			r=0; 
+		}
+		
+		var t1= 10*x;
+		var t2= 8*y;
+		var t3= 12*z;
+		var t4= 20*e;
+		var t5= 3*r;
+		
+		var tP=t1+t2+t3+t4+t5;
+		
+		
+		$('#tablaCarrito').html('<TABLE BORDER="1" id="tabla">'+
+							'<tr class="tr">'+
+								'<th>Producto</th>'+
+								'<th>Cantidad</th>'+
+								'<th>Precio Unitario</th>'+
+								'<th>Precio Total</th>'+
+							'</tr>'+							
+							'<tr class="tr">'+
+								'<td class="td">Pie de Limon</td>'+
+								'<td class="td">'+x+'</td>'+
+								'<td class="td">S/10.00</td>'+
+								'<td class="td">S/'+t1+'</td>'+
+							'</tr>'+							
+							'<tr class="tr">'+
+								'<td class="td">Alfajores de Maicena</td>'+
+								'<td class="td">'+y+'</td>'+
+								'<td class="td">S/8.00</td>'+
+								'<td class="td">S/'+t2+'</td>'+
+							'</tr>'+							
+							'<tr class="tr">'+
+								'<td class="td">Cheescake de Fresa</td>'+
+								'<td class="td">'+z+'</td>'+
+								'<td class="td">S/12.00</td>'+
+								'<td class="td">S/'+t3+'</td>'+
+							'</tr>'+							
+							'<tr class="tr">'+
+								'<td class="td">Crema Volteada</td>'+
+								'<td class="td">'+e+'</td>'+
+								'<td class="td">S/20.00</td>'+
+								'<td class="td">S/'+t4+'</td>'+
+							'</tr>'+
+							'<tr class="tr">'+
+								'<td class="td">Cupcake</td>'+
+								'<td class="td">'+r+'</td>'+
+								'<td class="td">S/3.00</td>'+
+								'<td class="td">S/'+t5+'</td>'+
+							'</tr>'+
+							'</table>'); 
+			
+			$('#tabla').addClass('td');
+			$('#tabla').addClass('tr'); 
+			
+			$('#tot1').attr('value', 'S/ '+tP+'.00');
+		
+			
+		
+			$('#a').click(function(){
+				$('#carrito').hide();
+				$('#containerCap').show();
+			});
+	});
+});
